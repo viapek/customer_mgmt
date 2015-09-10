c161db674127de85874b6793f606e03ec18245e9
Jeremy Watt <jwatt37@hotmail.com>
2015-09-10 21:42:41 +0900

    Really, stable forms

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
- root 'application#dashboard'
+ get 'admin', :to => 'access#index'
+ root 'application#index'
  match ':controller(/:action(/:id))', :via => [:get, :post]
 
   # The priority is based upon order of creation: first created -> highest priority.
